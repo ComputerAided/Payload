d5731f723a0af6ab70af2451702afc5379935432
ComputerAided <ComputerAided@users.noreply.github.com>
2016-05-15 21:21:18 -0600

    Create Payload.h

---
+++ Payload/Payload.h
@@ -0,0 +1,16 @@
+#ifndef Payload_h
+#define Payload_h
+
+//I2C/TWI Library
+#include <Wire.h>
+
+//Si7021 Library
+#include <SparkFun_Si7021.h>
+
+//SD Library
+#include <SD.h>
+
+//SPI Library
+#include <SPI.h>
+
+#endif
