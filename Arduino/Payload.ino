85b3ab77036968726febc8e3d403d1941de92e71
ComputerAided <ComputerAided@users.noreply.github.com>
2016-06-20 18:14:32 -0600

    Update Payload.ino
    Added GPS Stuff

--- Arduino/Payload.ino
+++ Arduino/Payload.ino
@@ -1,14 +1,10 @@
-
 /*****************************************************
  * Payload.ino
  * Written By Kenny H
  * THE SOFTWARE IS PROVIDED "AS-IS", AND "WITH ALL FAULTS", 
  * WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED.
- * Not yet finished
 *****************************************************/
 #include <Payload.h> // includes for I2C, SPI, SD, Si7021, BMP180 and SPI
-//output over Serial USB
-#define debug 0
 
 //Creates an instance for the Si7021
 HTU21D sensor;
@@ -17,44 +13,63 @@ HTU21D sensor;
 SFE_BMP180 pressure;
 
 //variable to hold weather recordings
-//SI7021 temp, Si7021 Humidity, BMP180 temp, BMP180 pressure
+//Si7021 temp, Si7021 Humidity, BMP180 temp, BMP180 pressure
 float weather[] = {0, 0, 0, 0};
 
 //GPS States
-enum states: uint8_t { 
-  NORMAL,
-  STARTED,
-  V, 
-  VT,
-  VTG,
-  G,
-  GG,
-  GGA
+enum states {
+  NO_READ,
+  READ
 };
 
-uint8_t state = NORMAL;
-
+uint8_t state = NO_READ;
 
 void setup () {
+  //enable sensors
   sensor.begin();
   pressure.begin();
+  
+  //enable USB
+  SerialUSB.begin(9600);
+  Serial1.begin(9600);
+  while (!Serial1 || !SerialUSB);
+
+  //Show setup is complete
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  delay(1000);
+  digitalWrite(13, HIGH);
+  SerialUSB.println("Setup Complete");
 }
 
 void loop () {
-  
+  switch (state) {
+    case READ: //tells to read from Serial1
+    {
+      String incomingString = Serial1.readStringUntil('*');
+      state = NO_READ;
+      SerialUSB.println(incomingString);
+      break;
+    } 
+    case NO_READ: //Does Other Stuff
+    {
+      pollSerial();
+      break;
+    }
+  }
 }
 
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 //function to read temp and humidity over Si7021
 void getWeather() {
 
 }
 
 //prints info from Si7021 - only for testing
-#if debug
 void printInfo() {
 
 }
-#endif
 
 //functions for SD
 void initSD () {
@@ -65,9 +80,14 @@ void writeToSD () {
 }
 
 //functions for GPS
-void getGPSData () {
-  Serial1.begin(9600);
-  
+void pollSerial () {
+  if (Serial1.available() > 0) {
+    if (Serial1.find('R')){
+      if (Serial1.find('M')) {
+        if (Serial1.find('C')) {
+          state = READ;
+        }
+      }
+    }
+  }
 }
-
-//
