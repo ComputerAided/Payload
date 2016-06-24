fe6f8371b479e4e48dd7eaf0595c4b498d1875fd
ComputerAided <ComputerAided@users.noreply.github.com>
2016-06-24 12:11:20 -0600

    Add files via upload

---
+++ Libraries/Payload/Payload.h
@@ -0,0 +1,21 @@
+#ifndef Payload_h
+#define Payload_h
+//Needed for any library
+#include "Arduino.h"
+
+//I2C/TWI Library
+#include <Wire.h>
+
+//Si7021 Library
+#include <SparkFunHTU21D.h>
+
+//BMP180
+#include <SFE_BMP180.h>
+
+//SD Library
+#include <SD.h>
+
+//SPI Library
+#include <SPI.h>
+
+#endif
