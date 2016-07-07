4efd649fafb6279615f9281334b2ef7c3d4e4c9e
GioScholar <gio.chrome69@gmail.com>
2016-07-07 14:21:21 -0600

    Update SD.ino

--- Test/SD/SD.ino
+++ Test/SD/SD.ino
@@ -18,10 +18,40 @@ float weather[3] = {32.2, 20, 32.3, 30.13};
 String GPSData = "$GPRMC, 161229.487, A, 3723.24756, N, 12158.34162, W, 0.13, 309.62, 120598, ,*10
 ";
 
+
+
+
+
+
+
 void setup () {
-  
+ Serial.print("Initializing SD card...");
+
+ 
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+   
+    return;
+  }
+  Serial.println("card initialized.");
+}
 }
 
 void loop () {
   
 }
+Void writeToSD
+
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    
+    Serial.println(dataString);
+  }
+  
+  else {
+    Serial.println("error opening datalog.txt");
+  }
